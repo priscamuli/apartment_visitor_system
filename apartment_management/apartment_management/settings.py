732d2b9e27f6d960d3bf04d7cca64ae812fa5910
priscamuli <priscakamanthe31@gmail.com>
2025-02-12 14:25:58 +0300

    added psycopg module

--- apartment_management/apartment_management/settings.py
+++ apartment_management/apartment_management/settings.py
@@ -79,13 +79,23 @@ WSGI_APPLICATION = 'apartment_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+ #       'NAME': BASE_DIR / 'db.sqlite3',
+ #   }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'apartment_management_giui',
+        'USER':'apartment_management_giui_user',
+        'PASSWORD':'bFQAIM4rB0r69XxYPgh5nrJqatmIP9mf',
+        'HOST':'dpg-cum7lein91rc739ru9g0-a.oregon-postgres.render.com',
+        'PORT':'5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
