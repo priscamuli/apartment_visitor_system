0e215d37f50232e8d25bb809fb85f7184886bd17
priscamuli <priscakamanthe31@gmail.com>
2025-02-12 20:58:25 +0300

    added db postgresql

--- apartment_management/apartment_management/settings.py
+++ apartment_management/apartment_management/settings.py
@@ -131,8 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Ensure this folder contains 'css/style.css'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 
